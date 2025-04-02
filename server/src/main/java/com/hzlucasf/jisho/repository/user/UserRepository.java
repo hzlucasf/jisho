fd4a953ff69459beacf092cd388c5be26ff2a63e
hzlucasf <hzlucasf@gmail.com>
2025-04-02 13:34:02 -0300

    feat(server): add UserRepository.java

---
+++ server/src/main/java/com/hzlucasf/jisho/repository/user/UserRepository.java
@@ -0,0 +1,9 @@
+package com.hzlucasf.jisho.repository.user;
+
+import com.hzlucasf.jisho.model.user.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
