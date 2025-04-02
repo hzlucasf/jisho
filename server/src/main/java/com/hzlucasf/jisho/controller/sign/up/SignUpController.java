6b3483f8bfe0ab9b786ac4c7592cf9528c2d49b1
hzlucasf <hzlucasf@gmail.com>
2025-04-02 19:31:00 -0300

    feat(server): update SignUpController.java

--- server/src/main/java/com/hzlucasf/jisho/controller/sign/up/SignUpController.java
+++ server/src/main/java/com/hzlucasf/jisho/controller/sign/up/SignUpController.java
@@ -1,9 +1,15 @@
 package com.hzlucasf.jisho.controller.sign.up;
 
+import com.hzlucasf.jisho.service.sign.up.SignUpService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/sign-up")
 public class SignUpController {
+    private final SignUpService service;
+
+    public SignUpController(SignUpService service) {
+        this.service = service;
+    }
 }
