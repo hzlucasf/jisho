74998b4f17ffff7111d1e1ec1fe4739157f6a35d
hzlucasf <hzlucasf@gmail.com>
2025-04-02 19:30:40 -0300

    feat(server): add SignUpController.java

---
+++ server/src/main/java/com/hzlucasf/jisho/controller/sign/up/SignUpController.java
@@ -0,0 +1,9 @@
+package com.hzlucasf.jisho.controller.sign.up;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/sign-up")
+public class SignUpController {
+}
