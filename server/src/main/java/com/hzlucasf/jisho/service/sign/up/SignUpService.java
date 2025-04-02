1fec8a1d4883e35523bcd2d869b4bde1ca494ed1
hzlucasf <hzlucasf@gmail.com>
2025-04-02 13:37:44 -0300

    feat(server): update SignUpService.java

--- server/src/main/java/com/hzlucasf/jisho/service/sign/up/SignUpService.java
+++ server/src/main/java/com/hzlucasf/jisho/service/sign/up/SignUpService.java
@@ -1,7 +1,13 @@
 package com.hzlucasf.jisho.service.sign.up;
 
+import com.hzlucasf.jisho.repository.user.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SignUpService {
+    private final UserRepository userRepository;
+
+    public SignUpService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 }
