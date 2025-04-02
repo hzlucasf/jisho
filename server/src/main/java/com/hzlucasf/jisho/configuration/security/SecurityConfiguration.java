bf7dc952accbaffed04b97b32d282e4a76343a5a
hzlucasf <hzlucasf@gmail.com>
2025-04-02 19:15:19 -0300

    feat(server): add SecurityConfiguration.java

---
+++ server/src/main/java/com/hzlucasf/jisho/configuration/security/SecurityConfiguration.java
@@ -0,0 +1,9 @@
+package com.hzlucasf.jisho.configuration.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+}
