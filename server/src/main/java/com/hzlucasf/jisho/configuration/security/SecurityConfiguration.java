9747a33da397720ed6b3fb5d179c667f061eebfc
hzlucasf <hzlucasf@gmail.com>
2025-04-02 19:38:23 -0300

    feat(server): update SecurityConfiguration.java

--- server/src/main/java/com/hzlucasf/jisho/configuration/security/SecurityConfiguration.java
+++ server/src/main/java/com/hzlucasf/jisho/configuration/security/SecurityConfiguration.java
@@ -2,13 +2,20 @@ package com.hzlucasf.jisho.configuration.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain getSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.build();
+    }
+
     @Bean
     public PasswordEncoder getPasswordEncoder() {
         return new Argon2PasswordEncoder(
