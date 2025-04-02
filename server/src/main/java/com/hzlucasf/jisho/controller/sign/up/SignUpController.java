3faf3d7367ad47bf7332aeb35a70af9925d1b2d0
hzlucasf <hzlucasf@gmail.com>
2025-04-02 19:33:25 -0300

    feat(server): update SignUpController.java

--- server/src/main/java/com/hzlucasf/jisho/controller/sign/up/SignUpController.java
+++ server/src/main/java/com/hzlucasf/jisho/controller/sign/up/SignUpController.java
@@ -1,6 +1,13 @@
 package com.hzlucasf.jisho.controller.sign.up;
 
+import com.hzlucasf.jisho.model.user.request.UserRequest;
+import com.hzlucasf.jisho.model.user.response.UserResponse;
 import com.hzlucasf.jisho.service.sign.up.SignUpService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +18,10 @@ public class SignUpController {
 
     public SignUpController(SignUpService service) {
         this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<UserResponse> signUp(@RequestBody @Valid UserRequest userRequest) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.signUp(userRequest));
     }
 }
