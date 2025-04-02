3e801f5944f632e0d87dac8dd917f142cc80a205
hzlucasf <hzlucasf@gmail.com>
2025-04-02 13:32:49 -0300

    feat(server): add UserResponse.java

---
+++ server/src/main/java/com/hzlucasf/jisho/model/user/response/UserResponse.java
@@ -0,0 +1,29 @@
+package com.hzlucasf.jisho.model.user.response;
+
+public class UserResponse {
+    private String id;
+
+    private String username;
+
+    public UserResponse(String id, String username) {
+        this.id = id;
+
+        this.username = username;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
