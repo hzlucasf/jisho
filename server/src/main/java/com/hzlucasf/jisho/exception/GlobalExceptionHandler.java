daaea166bb76f33e433a71507fad761692159d47
hzlucasf <hzlucasf@gmail.com>
2025-04-02 13:56:02 -0300

    feat(server): update GlobalExceptionHandler.java

--- server/src/main/java/com/hzlucasf/jisho/exception/GlobalExceptionHandler.java
+++ server/src/main/java/com/hzlucasf/jisho/exception/GlobalExceptionHandler.java
@@ -1,7 +1,18 @@
 package com.hzlucasf.jisho.exception;
 
+import com.hzlucasf.jisho.model.exception.response.ExceptionResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseBody
+    public ExceptionResponse exceptionHandler(Exception exception) {
+        return new ExceptionResponse("internal server error");
+    }
 }
