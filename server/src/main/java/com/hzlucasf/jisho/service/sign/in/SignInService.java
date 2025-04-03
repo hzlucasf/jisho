dcc6b7a464d80e7d7e3259ee070686407c876f51
hzlucasf <hzlucasf@gmail.com>
2025-04-02 22:15:44 -0300

    feat(server): update SignInService.java

--- server/src/main/java/com/hzlucasf/jisho/service/sign/in/SignInService.java
+++ server/src/main/java/com/hzlucasf/jisho/service/sign/in/SignInService.java
@@ -1,7 +1,13 @@
 package com.hzlucasf.jisho.service.sign.in;
 
+import com.hzlucasf.jisho.service.jwt.JwtService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SignInService {
+    private final JwtService jwtService;
+
+    public SignInService(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
 }
