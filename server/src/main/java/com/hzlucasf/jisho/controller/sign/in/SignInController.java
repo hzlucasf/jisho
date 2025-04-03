ab1f924543b70a5cac265facf6df198a2a9b9098
hzlucasf <hzlucasf@gmail.com>
2025-04-02 22:17:32 -0300

    feat(server): add SignInController.java

---
+++ server/src/main/java/com/hzlucasf/jisho/controller/sign/in/SignInController.java
@@ -0,0 +1,9 @@
+package com.hzlucasf.jisho.controller.sign.in;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/sign-in")
+public class SignInController {
+}
