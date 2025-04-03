20c1b45d67c55838193da6e35d590e28ad8e436c
hzlucasf <hzlucasf@gmail.com>
2025-04-02 22:04:01 -0300

    feat(server): update CustomUserDetailsService.java

--- server/src/main/java/com/hzlucasf/jisho/service/user/CustomUserDetailsService.java
+++ server/src/main/java/com/hzlucasf/jisho/service/user/CustomUserDetailsService.java
@@ -1,13 +1,35 @@
 package com.hzlucasf.jisho.service.user;
 
 import com.hzlucasf.jisho.repository.user.UserRepository;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CustomUserDetailsService {
+public class CustomUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
     public CustomUserDetailsService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var optionalUser = userRepository.findByUsername(username);
+
+        if (optionalUser.isEmpty()) {
+            throw new UsernameNotFoundException("username not found");
+        }
+
+        var user = optionalUser.get();
+
+        return User
+                .builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .roles(user.getRole().name())
+                .build();
     }
 }
