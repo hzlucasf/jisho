76cb7cd461514f0e04bd2eec32560f668e16f3f2
hzlucasf <hzlucasf@gmail.com>
2025-04-02 22:00:49 -0300

    feat(server): update CustomUserDetailsService.java

--- server/src/main/java/com/hzlucasf/jisho/service/user/CustomUserDetailsService.java
+++ server/src/main/java/com/hzlucasf/jisho/service/user/CustomUserDetailsService.java
@@ -1,7 +1,13 @@
 package com.hzlucasf.jisho.service.user;
 
+import com.hzlucasf.jisho.repository.user.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomUserDetailsService {
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 }
