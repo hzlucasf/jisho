f5cb1595861e4fd90ae1eb83b0f81993c0563565
hzlucasf <hzlucasf@gmail.com>
2025-04-03 11:34:30 -0300

    feat(server): update CorsConfiguration.java

--- server/src/main/java/com/hzlucasf/jisho/configuration/cors/CorsConfiguration.java
+++ server/src/main/java/com/hzlucasf/jisho/configuration/cors/CorsConfiguration.java
@@ -1,7 +1,14 @@
 package com.hzlucasf.jisho.configuration.cors;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfiguration {
+    @Bean
+    public WebMvcConfigurer getWebMvcConfigurer() {
+        return new WebMvcConfigurer() {
+        };
+    }
 }
