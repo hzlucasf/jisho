e02ab2cdd62a59492b37345debb57931e1119d50
hzlucasf <hzlucasf@gmail.com>
2025-04-02 22:18:24 -0300

    feat(server): update SignInController.java

--- server/src/main/java/com/hzlucasf/jisho/controller/sign/in/SignInController.java
+++ server/src/main/java/com/hzlucasf/jisho/controller/sign/in/SignInController.java
@@ -1,6 +1,11 @@
 package com.hzlucasf.jisho.controller.sign.in;
 
+import com.hzlucasf.jisho.model.jwt.response.JwtResponse;
 import com.hzlucasf.jisho.service.sign.in.SignInService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +16,10 @@ public class SignInController {
 
     public SignInController(SignInService service) {
         this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<JwtResponse> signIn(Authentication authentication) {
+        return ResponseEntity.status(HttpStatus.OK).body(service.signIn(authentication));
     }
 }
