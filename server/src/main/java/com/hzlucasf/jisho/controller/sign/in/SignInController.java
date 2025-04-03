945d48913c700ed88aed3a93a362cc93eb87115c
hzlucasf <hzlucasf@gmail.com>
2025-04-02 22:17:54 -0300

    feat(server): update SignInController.java

--- server/src/main/java/com/hzlucasf/jisho/controller/sign/in/SignInController.java
+++ server/src/main/java/com/hzlucasf/jisho/controller/sign/in/SignInController.java
@@ -1,9 +1,15 @@
 package com.hzlucasf.jisho.controller.sign.in;
 
+import com.hzlucasf.jisho.service.sign.in.SignInService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/sign-in")
 public class SignInController {
+    private final SignInService service;
+
+    public SignInController(SignInService service) {
+        this.service = service;
+    }
 }
