ac99f1fa47362afb2b8ca01df851a1a86fddc5ce
hzlucasf <hzlucasf@gmail.com>
2025-04-02 22:01:32 -0300

    feat(server): update UserRepository.java

--- server/src/main/java/com/hzlucasf/jisho/repository/user/UserRepository.java
+++ server/src/main/java/com/hzlucasf/jisho/repository/user/UserRepository.java
@@ -4,7 +4,11 @@ import com.hzlucasf.jisho.model.user.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
     boolean existsByUsername(String username);
+
+    Optional<User> findByUsername(String username);
 }
